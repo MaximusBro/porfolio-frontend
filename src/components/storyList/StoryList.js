7d664285c0544fe496f9432154b03b3e2922e488
maximusbro <shidlovskiy.maksimka@gmail.com>
2022-12-27 04:46:47 +0100

    Stories from json-server

--- src/components/storyList/StoryList.js
+++ src/components/storyList/StoryList.js
@@ -1,44 +1,42 @@
+import { useState, useEffect } from "react";
+import { useHttp } from "../hooks/http.hook";
 import "../storyList/storyList.scss";
 const StoryList = () => {
+	const [stories, setStories] = useState([]);
+
+	const { request } = useHttp();
+
+	useEffect(() => {
+
+		request("http://localhost:3001/stories")
+			.then(data => setStories(data))
+			.catch(error => { throw new Error("Failude to get stories", error) })
+		// eslint-disable-next-line
+	}, []);
+
+	const renderStories = (arr) => {
+		return arr.filter((item, index) => index < 4).map(({ ...props }, index) => {
+			return <Story key={index} {...props} />
+		})
+	}
+	const elementsStory = renderStories(stories);
 	return (
 		<div className="story">
 			<div className="story-column">
-				<div className="story-column-card">
-					<div className="story-column-card__title">
-						<h2>Переехал в новую
-							квартиру</h2>
-					</div>
-					<div className="story-column-card__date">
-						<p>11.09.2020</p>
-					</div>
-				</div>
-				<div className="story-column-card">
-					<div className="story-column-card__title">
-						<h2>Переехал в новую
-							квартиру</h2>
-					</div>
-					<div className="story-column-card__date">
-						<p>11.09.2020</p>
-					</div>
-				</div>
-				<div className="story-column-card">
-					<div className="story-column-card__title">
-						<h2>Переехал в новую
-							квартиру</h2>
-					</div>
-					<div className="story-column-card__date">
-						<p>11.09.2020</p>
-					</div>
-				</div>
-				<div className="story-column-card">
-					<div className="story-column-card__title">
-						<h2>Переехал в новую
-							квартиру</h2>
-					</div>
-					<div className="story-column-card__date">
-						<p>11.09.2020</p>
-					</div>
-				</div>
+				{elementsStory}
+
+			</div>
+		</div>
+	)
+}
+const Story = ({ title, date }) => {
+	return (
+		<div className="story-column-card">
+			<div className="story-column-card__title">
+				<h2>{title}</h2>
+			</div>
+			<div className="story-column-card__date">
+				<p>{date}</p>
 			</div>
 		</div>
 	)
